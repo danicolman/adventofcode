34b40078360e5a6d28ffa20c3312858cdae2a93c
danicolman <danitcolman@gmail.com>
2023-12-02 11:55:11 +0000

    Why doesn't this work?

---
+++ Day_2/Day2.py
@@ -0,0 +1,45 @@
+f = open("input.txt", "r")
+
+rlim = 12
+glim = 13
+blim = 14
+
+possible_games = []
+impossible_games = []
+possible_total = 0
+
+for i in f:
+    gid = int(i.split(":")[0].split()[1])
+    games = i.strip().split(": ")[1].split("; ")
+
+    possible = True
+
+    nums = i.strip().split(": ")[1].replace(";", ",").split(", ")
+
+    for num in nums:
+        if int(num.split(" ")[0]) > 14:
+            possible = False
+            print("Game ", gid, " Exceeds blue max")
+            break
+        elif int(num.split(" ")[0]) == 14:
+            if num.split(" ")[1] not in ["blue"]:
+                possible = False
+                print("Game ", gid, " Exceeds red/green max")
+            break
+        elif int(num.split(" ")[0]) == 13:
+            if num.split(" ")[1] not in ["blue", "green"]:
+                possible = False
+                print("Game ", gid, " Exceeds red max")
+            break
+        else:
+            print(gid, ": ", num)
+
+    if possible:
+        possible_games.append(gid)
+        possible_total += gid
+    else:
+        impossible_games.append(gid)
+
+print(possible_games)
+print(impossible_games)
+print(possible_total)
